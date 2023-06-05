6e0c716fbf2ec9b384293e2db7983d9c40c511d5
Leonardo Tolaba <leonardotolaba.20@gmail.com>
2023-06-04 23:22:46 -0300

    Terminando proyecto

--- projects/04-prueba-tecnica/src/App.jsx
+++ projects/04-prueba-tecnica/src/App.jsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+
+const CAT_ENDPOINT_RANDOM_FACT = "https://catfact.ninja/fact";
+// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
+const CAT_PREFIX_IMAGE_URL = "https://cataas.com";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [fact, setFact] = useState();
+  const [image, setImage] = useState();
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then((res) => res.json())
+      .then((data) => {
+        const { fact } = data;
+        setFact(fact);
+      })
+    /*  FETCH CON ASYNC Y AWAIT  */
+    // async function getRandonFact () {
+    //   const res = await fetch(CAT_ENDPOINT_RANDOM_FACT)
+    //   const json = await res.json()
+    //   setFact(json.fact)
+    // }
+
+    // getRandonFact()
+  }, []);
+
+  useEffect(() => {
+      if(!fact) return
+        //const firstWord = fact.split(' ').slice(0,3).join(' '); //Agarra las 3 primeras palabras
+        const firstWord = fact.split(" ", 3).join(" "); //Hace lo mismo peros implificado
+        fetch(
+          `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
+        )
+          .then((res) => res.json())
+          .then((response) => {
+            const { url } = response;
+            setImage(`${url}`);
+          });
+  }, [fact])
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main>
+      <h1>App de gatitos</h1>
+      {fact && <p>{fact}</p>}
+      {image && (
+        <img
+          src={`${CAT_PREFIX_IMAGE_URL}${image}`}
+          alt={`Image extracted using the first three words for ${fact}`}
+        />
+      )}
+    </main>
+  );
 }
 
-export default App
+export default App;
